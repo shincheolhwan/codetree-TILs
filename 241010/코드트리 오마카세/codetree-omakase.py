253a69b76807279dd81a4c766083a7aebef72eb6
shincheolhwan <78342372+shincheolhwan@users.noreply.github.com>
2024-10-10 22:16:12 +0900

    Title: 코드트리 오마카세, Time: 998ms, Memory: 55MB, Status: Passed - CodeTree

--- 241010/코드트리 오마카세/codetree-omakase.py
+++ 241010/코드트리 오마카세/codetree-omakase.py
@@ -1,56 +1,38 @@
 # 2024.10.10 20:01 시작
+# 못 풀었음
 import sys
 
 # sys.stdin = open('input.txt', 'r')
 
 
-def eat_sushi(t):
+def eat_sushi():
     count = 0
     names = list(customers.keys())
 
     for name in names:
         customer = customers[name]
+        dishes = sushi[name]
+
         sit_t = customer[0]
         sit_x = customer[1]
+        exit_time = -1
 
-        # 나온 초밥이 있을 때
-        if name in sushi:
-            dishes = sushi[name]
-            remain_dishes = []
+        for dish in dishes:
+            dish_t = dish[0]
+            dish_x = dish[1]
 
-            for dish in dishes:
-                dish_t = dish[0]
-
-                if dish_t < sit_t:
-                    start_x = dish[1] + sit_t - dish_t
-                    end_x = dish[1] + t - dish_t
-                else:
-                    start_x = dish[1]
-                    end_x = dish[1] + t - dish_t
-
-                temp = L * (start_x // L)
-                # print(f"start_x:{start_x} end_x:{end_x} sit_x:{sit_x}")
-                if (
-                        (end_x - start_x >= L)
-                        or (start_x - temp <= sit_x <= end_x - temp)
-                        or (start_x - temp <= sit_x + L <= end_x - temp)
-                ):
-                    count += 1
-                    customer[2] -= 1
-
-                    if customer[2] == 0:
-                        del customers[name]
-                else:
-                    remain_dishes.append(dish)
-
-            if len(remain_dishes) > 0:
-                sushi[name] = remain_dishes
+            # 음식이 먼저 나왔을 때
+            if dish_t < sit_t:
+                start_x = (dish_x + sit_t - dish_t) % L
+                eat_time = sit_t + (sit_x + L - start_x) % L
+            # 음식이 나중에 나왔을 때
             else:
-                del sushi[name]
+                start_x = dish[1]
+                eat_time = dish_t + (sit_x + L - start_x) % L
+            exit_time = max(exit_time, eat_time)
+            queries.append((111, eat_time))
 
-        # 나온 초밥이 없을 때
-        else:
-            continue
+        queries.append((222, exit_time))
 
     return count
 
@@ -61,7 +43,7 @@ L, Q = map(int, input().split())
 
 sushi = {}
 customers = {}
-dish_count = 0
+queries = []
 
 for _ in range(Q):
     commands = input().split()
@@ -70,21 +52,38 @@ for _ in range(Q):
     # 초밥 만들기
     if code == 100:
         t, x, name = int(commands[1]), int(commands[2]), commands[3]
-
         if name not in sushi:
             sushi[name] = []
             pass
         # 시간, 위치
         sushi[name].append([t, x])
-        dish_count += 1
+        queries.append((100, t))
     # 손님 입장
     elif code == 200:
         t, x, name, n = int(commands[1]), int(commands[2]), commands[3], int(commands[4])
         # 시간, 위치, 개수
         customers[name] = [t, x, n]
+        queries.append((200, t))
     # 사진 촬영
     elif code == 300:
         t = int(commands[1])
-        eat_count = eat_sushi(t)
-        dish_count -= eat_count
-        print(len(customers), dish_count)
+        queries.append((300, t))
+
+eat_sushi()
+queries.sort(key=lambda query: (query[1], query[0]))
+
+dish_count = 0
+customer_count = 0
+
+for q in queries:
+    # print(q[0], q[1])
+    if q[0] == 100:
+        dish_count += 1
+    elif q[0] == 111:
+        dish_count -= 1
+    elif q[0] == 200:
+        customer_count += 1
+    elif q[0] == 222:
+        customer_count -= 1
+    elif q[0] == 300:
+        print(f"{customer_count} {dish_count}")
